939ef2a8eb950cf390fba676c039c9ea675b71ca
lorisfo <loris-fo@outlook.com>
2015-01-27 11:23:26 +0000

    Asking for user input

--- directory.rb
+++ directory.rb
@@ -1,26 +1,21 @@
 # Let's put all students into an array
-students = [
-{:names => "Erik Griffin", :cohort => :february},
-{:names => "Julian Veling", :cohort => :february},
-{:names => "Alex Parkinson", :cohort => :february},
-{:names => "Louise Beh", :cohort => :february},
-{:names => "Sean Haughton", :cohort => :february},
-{:names => "Edward Brown", :cohort => :february},
-{:names => "Kate Beavis", :cohort => :february},
-{:names => "Thomas Strothjohann", :cohort => :february},
-{:names => "Kevin Lanzon", :cohort => :february},
-{:names => "Yannick B.", :cohort => :february},
-{:names => "Meads Chalcroft", :cohort => :february},
-{:names => "Gabriel Arafa", :cohort => :february},
-{:names => "Sebastien Pires", :cohort => :february},
-{:names => "Jordan Poultron", :cohort => :february},
-{:names => "Tom coakes", :cohort => :february},
-{:names => "Vanessa Virgitti", :cohort => :february},
-{:names => "Paul Fitzgerald", :cohort => :february},
-{:names => "Carrie Christenson", :cohort => :february},
-{:names => "Diego Romero", :cohort => :february},
-{:names => "Loris Fochesato", :cohort => :february}
-] 
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create an empty array
+    students = []
+    # get the first name
+    name = gets.chomp
+    #while the name is not empty, repeat this code
+    while !name.empty? do# add the student hash to the array
+      students << {:name => name, :cohort => :february}
+      puts "Now we have #{students.length} students"
+      # get another name from the user
+      name = gets.chomp
+    end
+    # return the array of students
+    students
+ end
 
 def print_header
   puts "The students of my cohort at Makers Academy"
@@ -29,7 +24,7 @@ end
 
 def print(students)
   students.each do |student| 
-  	puts "#{student[:names]} (#{student[:cohort]} cohort)"
+  	puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -39,6 +34,7 @@ def print_footer(names)
 end
 
 # Nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
