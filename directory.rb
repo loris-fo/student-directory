d8a13cc721626659e803cb4e9e4b3bbe8a93e116
lorisfo <loris-fo@outlook.com>
2015-01-27 12:16:40 +0000

    changed 'each' interation to 'while'

--- directory.rb
+++ directory.rb
@@ -23,8 +23,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-  	puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?("A")
+  i=0
+    while i<students.length
+  	puts "#{students[0]} (#{students[1]} cohort)"
+  	i+=1
   end
 end
 
