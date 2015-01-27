a65783ea9c1a2cbdaf569a5492bd064e03fc474a
lorisfo <loris-fo@outlook.com>
2015-01-27 13:45:12 +0000

    creating interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,27 @@
+def interactive_menu
+	students = []
+	loop do
+		# 1. print the menu and ask the user what to do
+		puts "1. Input the students"
+		puts "2. Show the students"
+		puts "9. Exit"
+		# 2. read the input and save it into a variable
+		selection = gets.chomp
+		# 3. do what the user has asked
+		case selection
+		when "1"
+			students = input_students
+		when "2"
+			print_header
+			print(students)
+			print_footer(students)
+		when "9"
+			exit
+		else
+			puts "I don't know what you meant, try again"
+		end
+	end
+end
 # Let's put all students into an array
 def input_students
 	puts "Please enter the names, country of birth of the students and their cohort"
@@ -41,8 +65,8 @@ def print_footer(names)
 end
 
 # Nothing happens until we call the methods
+interactive_menu
 students = input_students
 print_header
 print(students)
 print_footer(students)
-
